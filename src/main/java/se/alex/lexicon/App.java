0fc69e0a109cef216539394b3034942d4aff2884
Alex Youssef <Alex.youssef@live.com>
2024-08-29 00:36:30 +0200

    Initial commit - Added MySQL connection and task querying

---
+++ src/main/java/se/alex/lexicon/App.java
@@ -0,0 +1,21 @@
+package se.alex.lexicon;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App {
+    public static void main(String[] args) {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM tasks"); // Querying the tasks table
+
+            while (resultSet.next()) {
+                System.out.println("Title: " + resultSet.getString("title") + ", Description: " + resultSet.getString("description"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/se/alex/lexicon/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package se.alex.lexicon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/SQL_ToDoIt";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";  // This is your MySQL password
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
