3829fb22d7372ab91b5ba85099f0ce549a1672f9
Alex Youssef <Alex.youssef@live.com>
2024-08-29 00:44:17 +0200

    Initial commit - Mysql

--- src/main/java/se/alex/lexicon/App.java
+++ src/main/java/se/alex/lexicon/App.java
@@ -9,7 +9,7 @@ public class App {
     public static void main(String[] args) {
         try (Connection connection = DatabaseConnection.getConnection()) {
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM tasks"); // Querying the tasks table
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM tasks");
 
             while (resultSet.next()) {
                 System.out.println("Title: " + resultSet.getString("title") + ", Description: " + resultSet.getString("description"));
